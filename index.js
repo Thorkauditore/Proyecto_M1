62e1adf8ebbfd488cca1e2692012cfa63212bd8b
Thorkauditore <gorkaetxean@gmail.com>
2022-10-11 16:28:25 +0200

    cambio

--- index.js
+++ index.js
@@ -8,3 +8,19 @@ let prueba5
 let prueba6
 
 let prueba7
+
+
+
+
+
+
+
+
+let numeros = [0, 1, 2, 3, 4, 5];
+        
+for (let i = 0; i < numeros.length; i++) {
+    console.log(numeros[i])
+    
+}
+
+console.log("---------------------")
