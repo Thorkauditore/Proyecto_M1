0d1aaa3d15846b2291cd6c454ad269073a5886e8
JorJim05 <jorge.jimenez.95.jj@gmail.com>
2022-10-13 17:07:46 +0200

    cambios css

---
+++ FormularioJS.js
@@ -0,0 +1,6 @@
+document.querySelector("select").addEventListener("change", function () {
+    //alert("He hecho click")
+    alert("Has elegido " + (document.querySelector("select").value));
+
+
+})
