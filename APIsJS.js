2c360ee39fac5480942e602f89fb45f191790c33
Thorkauditore <gorkaetxean@gmail.com>
2022-10-14 10:52:03 +0200

    cambios

--- APIsJS.js
+++ APIsJS.js
@@ -7,7 +7,7 @@ const options = {
   headers: {
     'X-RapidAPI-Key': '0da16c44fbmsh7d65b9193e777b5p1c1087jsnc7a35e22321b',
     'X-RapidAPI-Host': 'nba-stats4.p.rapidapi.com'
-  } 
+  }
 }
 
 fetch('https://nba-stats4.p.rapidapi.com/teams/?per_page=50&page=1', options)
@@ -23,10 +23,10 @@ fetch('https://nba-stats4.p.rapidapi.com/teams/?per_page=50&page=1', options)
 document.querySelector("input[type='submit']").addEventListener("click", (e) => {
   e.preventDefault()
   let datosEquipo = document.querySelector("select").value
+  console.log(datosEquipo);
   document.querySelector("#fullName").innerHTML = `<h2>${teams[datosEquipo].full_name}</h2>`
   document.querySelector("#citys").innerHTML = `<h4>Ciudad; ${teams[datosEquipo].city}</h4>`
   document.querySelector("#yearsold").innerHTML = `<h3>Año de fundación ${teams[datosEquipo].year_founded}</h3>`
-  if (datosEquipo == 1) {
-    document.querySelector("#img").src = `<img src="./https://en.wikipedia.org/wiki/Atlanta_Hawks#/media/File:Atlanta_Hawks_logo.svg" alt="vaya">`
-  }
+  document.querySelector("#img").innerHTML = `<img src="./teams/${teams[datosEquipo].abbreviation}.png" alt="vaya">`
+
 })
