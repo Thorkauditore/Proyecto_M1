d5547457742c10fdf3ffca606413c19bc548153a
JorJim05 <jorge.jimenez.95.jj@gmail.com>
2022-10-14 11:43:56 +0200

    OK

--- FormularioJS.js
+++
@@ -1,6 +0,0 @@
-document.querySelector("select").addEventListener("change", function () {
-    //alert("He hecho click")
-    alert("Has elegido " + (document.querySelector("select").value));
-
-
-})
