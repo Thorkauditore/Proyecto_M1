fa68f0e394cefe5b617593d2c997eba4e7253241
Thorkauditore <gorkaetxean@gmail.com>
2022-10-15 19:43:46 +0200

    nueva todo

--- APIsJS.js
+++ APIsJS.js
@@ -14,8 +14,8 @@ fetch('https://nba-stats4.p.rapidapi.com/teams/?per_page=50&page=1', options)
   .then(response => response.json())
   .then((response) => {
     teams = response // El array de obejtos.
-   
-   console.log(response);
+
+    console.log(response);
     response.forEach((team, i) => {
       select.innerHTML += `<option value="${i}">${team.abbreviation}</option>`;
     });
@@ -28,6 +28,8 @@ document.querySelector("input[type='submit']").addEventListener("click", (e) => 
   document.querySelector("#fullName").innerHTML = `<h2>${teams[datosEquipo].full_name}</h2>`
   document.querySelector("#citys").innerHTML = `<h4>Ciudad; ${teams[datosEquipo].city}</h4>`
   document.querySelector("#yearsold").innerHTML = `<h3>Año de fundación ${teams[datosEquipo].year_founded}</h3>`
-  document.querySelector("#imgDatos").innerHTML = `<img src="./teams/${teams[datosEquipo].abbreviation}.png" alt="vaya">`
+  document.querySelector("#imgTeams").innerHTML = `<img src="./teams/${teams[datosEquipo].abbreviation}.png" alt="vaya">`
+  document.querySelector("#imgPlayers").innerHTML = `<img src="./jugadores/${teams[datosEquipo].abbreviation}.png" alt="vaya">`
+
 
 })
